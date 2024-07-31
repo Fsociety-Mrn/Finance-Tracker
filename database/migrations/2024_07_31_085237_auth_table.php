6a4154de0f39b968b4ceaccd9e20cd212b964d31
Art Lisboa <lisboamillen30@gmail.com>
2024-07-31 17:16:13 +0800

    updated

---
+++ database/migrations/2024_07_31_085237_auth_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // create auth table schema
+        Schema::create('auth_table',function(Blueprint $table){
+            $table->id('uid');
+            $table->string('FullName');
+            $table->integer('Age')->length(11);
+            $table->date('Birthday');
+            $table->string('Username');
+            $table->string('Email');
+            $table->string('Password');
+            $table->timestamp('Account_created')->useCurrent()->nullable();
+            $table->timestamp('Account_updated')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('auth_table');
+    }
+};
